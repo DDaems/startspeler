bef5c74454696a2f354f1cf6f77b274a13b50606
NSterckx <r0247496@users.noreply.github.com>
2021-04-14 22:32:28 +0200

    Initial Update
    Nugget packages
    Model
    Connection string + schema

---
+++ Startspeler/StartspelerMVC/Models/Evenement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartspelerMVC.Models
+{
+    public class Evenement
+    {
+        public int EvenementID { get; set; }
+        public string Titel { get; set; }
+
+        public DateTime Startdatum { get; set; }
+
+        public string Beschrijving { get; set; }
+
+        public float Kostprijs { get; set; }
+
+        public int Max_Deelnemers { get; set; }
+
+        public DateTime Einddatum { get; set; }
+
+        public int EvenementTypeID { get; set; }
+
+        public ICollection<Inschrijving> Inschrijvingen { get; set; }
+    }
+}
---
+++ Startspeler/StartspelerMVC/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartspelerMVC.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public DateTime Geboortedatum { get; set; }
+
+        public int Pincode { get; set; }
+
+        public string token { get; set; }
+
+        public Boolean Admin { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Inschrijving> Inschrijvingen { get; set; }
+    }
+}
