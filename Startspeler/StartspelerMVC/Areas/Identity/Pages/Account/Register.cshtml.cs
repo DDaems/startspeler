2e5b16afa3c18ced7b432be8b53b9d25d3e9fba3
Dieter Daems <daemsdieter@gmail.com>
2021-04-25 10:14:34 +0200

    Setup basic view and model of registratie form

--- Startspeler/StartspelerMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Startspeler/StartspelerMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,19 +47,32 @@ namespace StartspelerMVC.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "Naam")]
+            public string Achternaam { get; set; }
+
+            [Required]
+            [Display(Name = "Voornaam")]
+            public string Voornaam { get; set; }
+
+            [Required]
+            [DataType(DataType.Date, ErrorMessage = "* geen geldige datum")]
+            [Display(Name = "Geboortedatum")]
+            public string Geboortedatum { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(4, ErrorMessage = "De pincode moet bestaan uit {2} cijfers", MinimumLength = 4)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Pincode")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Bevestig pincode")]
+            [Compare("Password", ErrorMessage = "De pincode komt niet overeen.")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -75,7 +88,12 @@ namespace StartspelerMVC.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User {
+                    Achternaam = Input.Achternaam,
+                    Voornaam = Input.Voornaam,
+                    Geboortedatum = Convert.ToDateTime(Input.Geboortedatum),
+                    UserName = Input.Email, 
+                    Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
@@ -89,8 +107,8 @@ namespace StartspelerMVC.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Bevesting je e-mail",
+                        $"Alstublieft bevestig uw account door <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>op mij te klikken</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
