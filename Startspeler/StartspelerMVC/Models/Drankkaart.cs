05ae354b73c5318597c4d924a67d308c3446ea3c
Dieter Daems <daemsdieter@gmail.com>
2021-04-27 19:47:55 +0200

    Add propertys to drankkaart

--- Startspeler/StartspelerMVC/Models/Drankkaart.cs
+++ Startspeler/StartspelerMVC/Models/Drankkaart.cs
@@ -5,13 +5,18 @@ using System.Threading.Tasks;
 
 namespace StartspelerMVC.Models
 {
+    
     public class Drankkaart
     {
         public int DrankkaartID { get; set; }
 
         //public int userID { get; set; }
 
-        public int Aantal_Verbruikt { get; set; }
+        public int Aantal_beschikbaar { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime Aankoopdatum { get; set; }
 
         public int DrankkaartTypeID { get; set; }
     }
