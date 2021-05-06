540f54a80b2c36b5d9c845065982ed925b17dd5b
NSterckx <r0247496@users.noreply.github.com>
2021-05-06 22:06:50 +0200

    numpad gemaakt op bevestigbestelling.

---
+++ Startspeler/StartspelerMVC/Migrations/20210506184725_AanpassingBestelling.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StartspelerMVC.Migrations
+{
+    public partial class AanpassingBestelling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                schema: "SS",
+                table: "Bestellingen",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                schema: "SS",
+                table: "Bestellingen");
+        }
+    }
+}
